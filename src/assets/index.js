08e9292a0fa32c6c24d36f3f752b99c1e794b937
Aime Pelozo <aime.pelozo@celeri.app>
2023-04-16 11:21:34 -0300

    clean, remove hero background

--- src/assets/index.js
+++ src/assets/index.js
@@ -8,7 +8,6 @@ import menu from "./menu.svg"
 import close from "./close.svg"
 
 import css from "./tech/css.png"
-import docker from "./tech/docker.png"
 import figma from "./tech/figma.png"
 import git from "./tech/git.png"
 import html from "./tech/html.png"
@@ -21,18 +20,11 @@ import tailwind from "./tech/tailwind.png"
 import typescript from "./tech/typescript.png"
 import threejs from "./tech/threejs.svg"
 
-import meta from "./company/meta.png"
-import shopify from "./company/shopify.png"
-import starbucks from "./company/starbucks.png"
-import tesla from "./company/tesla.png"
 import cocos from "./company/cocos.png"
 import celeri from "./company/celeri.png"
 import delta from "./company/delta.png"
 import tsa from "./company/tsa.png"
 
-import carrent from "./carrent.png"
-import jobit from "./jobit.png"
-import tripguide from "./tripguide.png"
 import todovue from "./todovue.png"
 import memorygame from "./memorygame.png"
 import monsape from "./monsape.png"
@@ -48,7 +40,6 @@ export {
     menu,
     close,
     css,
-    docker,
     figma,
     git,
     html,
@@ -60,13 +51,6 @@ export {
     tailwind,
     typescript,
     threejs,
-    meta,
-    shopify,
-    starbucks,
-    tesla,
-    carrent,
-    jobit,
-    tripguide,
     cocos,
     celeri,
     delta,
