ff2c5e1f8de2c085e4028c5f8f2b4f737fe745f3
Aime Pelozo <aime.pelozo@celeri.app>
2023-04-16 11:36:06 -0300

    last try

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import { ComputersCanvas } from "./canvas"
 const Hero = () => {
     return (
         <section className={`relative w-full h-screen mx-auto`}>
-            <div
+           {/*  <div
                 className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
             >
                 <div className="flex flex-col justify-center items-center mt-5">
@@ -25,10 +25,10 @@ const Hero = () => {
                     </p>
                 </div>
             </div>
-
+ */}
             <ComputersCanvas />
 
-            <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+       {/*      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
                 <a href="#about">
                     <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
                         <motion.div
@@ -44,7 +44,7 @@ const Hero = () => {
                         />
                     </div>
                 </a>
-            </div>
+            </div> */}
         </section>
     )
 }
